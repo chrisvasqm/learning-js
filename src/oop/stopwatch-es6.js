79a2e0609ac0aa3e0b9ad06333c48f572a6abfcd
chrisvasqm <christianvasquezmelo@gmail.com>
2023-01-08 13:58:11 -0400

    Add stopwatch example using ES6 syntax

---
+++ src/oop/stopwatch-es6.js
@@ -0,0 +1,34 @@
+class Stopwatch {
+  #startTime = null;
+  #endTime = null;
+  #duration = 0;
+  #isRunning = false;
+
+  start() {
+    if (this.#isRunning) throw new Error("Stopwatch has already been started");
+
+    this.#isRunning = true;
+    this.#startTime = new Date();
+  }
+
+  stop() {
+    if (!this.#isRunning) throw new Error("Stopwatch is already stopped.");
+
+    this.#isRunning = false;
+    this.#endTime = new Date();
+    const seconds =
+      (this.#endTime.getTime() - this.#startTime.getTime()) / 1000;
+    this.#duration += seconds;
+  }
+
+  reset() {
+    this.#startTime = null;
+    this.#endTime = null;
+    this.#duration = 0;
+    this.#isRunning = false;
+  }
+
+  get getDuration() {
+    return this.#duration;
+  }
+}
