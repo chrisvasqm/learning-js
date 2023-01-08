f140de6305b5f8e22c25f39a1067c2640dc12182
chrisvasqm <christianvasquezmelo@gmail.com>
2023-01-07 21:46:15 -0400

    Add more Arrays examples

--- topics/basics/arrays.js
+++ topics/basics/arrays.js
@@ -1,5 +1,12 @@
-const colors = ['Red', 'Yellow', 'Blue', 'Black'];
-
+const colors = ["Red", "Yellow", "Blue", "Black"];
 colors.forEach(color => {
-    console.log(color);
+  console.log(color);
 });
+
+const shortNamedColors = colors.filter(color => color.length <= 3);
+console.log(
+  `The colors that have 3 characters or less are: ${shortNamedColors}`
+);
+
+const boldedColors = colors.map(color => `<b>${color}</b>`);
+console.log(boldedColors);
